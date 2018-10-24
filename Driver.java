95f7da8ec26bd1bde2111e38ec3aae349c4d95bb
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-10-23 21:24:06 -0400

    fixed first few methods, tested them

---
+++ Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+  public static void main(String[] args) {
+    SuperArray list = new SuperArray();
+
+    for (int x = 0; x < list.length(); x++) {
+      list.add("hi");
+    }
+    System.out.println(list); //should print [hi, hi, hi, hi, hi, hi, hi, hi, hi, hi]
+    list.set(3, "hello");
+    System.out.println(list);
+
+  }
+}
--- SuperArray.java
+++ SuperArray.java
@@ -16,38 +16,55 @@
 
    public int size() {
      return size;
+   }
 
+   public int length() {
+     return data.length;
    }
 
    public boolean isEmpty() {
      return size == 0;
    }
 
-   public add(String item) { // should have return type boolean???
+   public boolean add(String item) { // should have return type boolean???
 /*     if (this.size > this.data.length) {
        this.resize();
      } */
       this.data[this.size] = item;
+      this.size++;
+      return true;
    }
 
    public String toString(){
+     if (size == 0) return "[]";
      String output = "[";
      for (int x = 0; x < this.size; x++) {
-       output += this.data[x]; + ", ";
+       output += this.data[x] + ", ";
      }
      return output.substring(0, output.length()-2) + "]";
    }
 
    public String toStringDebug() {
+     if (size == 0) return "[]";
      String output = "[";
      for (int x = 0; x < this.data.length; x++) {
-       output += this.data[x]; + ", ";
+       output += this.data[x] + ", ";
      }
      return output.substring(0, output.length()-2) + "]";
    }
 
    public String get(int index) {
-     
+     if (index < 0 || index >= size) return null;
+     else {return this.data[index];}
+   }
+
+   public String set(int index, String item) {
+     if (index < 0 || index >= size) return null;
+     else {
+       String old = this.data[index];
+       this.data[index] = item;
+       return old;
+     }
    }
 
 
