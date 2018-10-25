8e9b957932aaf38d0d71b5bf26055cd2a51e04c6
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-10-25 19:02:16 -0400

    tested contains

--- Driver.java
+++ Driver.java
@@ -11,35 +11,43 @@ public class Driver {
     System.out.println("Testing size with empty list, should return 0: ");
     System.out.println(list.size());
     System.out.println("----------------");
+
     System.out.println("Testing add(String)");
     System.out.println("Should return true: \n" + list.add("hello"));
     for (int x = 0; x < 99; x++) {
         list.add("hi");
       }
-    System.out.println();
+    System.out.println("Should return [hello, hi, hi...]: ");
     System.out.println(list);
 
-    System.out.println(list.get(0)); //should print hello
-    System.out.println(list.get(5)); //should print hi
-    System.out.println(list.get(1000)); //should print null
+    System.out.println("\nShould print \"hello\": \n" + list.get(0)); //should print hello
+    System.out.println("\nShould print \"hi\": \n" +list.get(5)); //should print hi
+    System.out.println("\nShould print \"Error: Index out of range\" and return null: \n" +list.get(1000)); //should print null
 
-    System.out.println("Testing clear; should return []");
+    System.out.println("\nTesting clear; should return []");
     list.clear();
     System.out.println(list);
 
-    System.out.println("Testing set at index");
-    System.out.println(list.set(10, "no"));
+    System.out.println("\nTesting set at index");
+    System.out.println("\nShould print \"Error: Index out of range\" and return null: \n" +list.set(10, "no"));
     for (int x = 0; x < 25; x++) {
         list.add("hi");
       }
-    System.out.println(list);
-    System.out.println(list.set(10, "bye")); //should return "hi"
+    System.out.println(list + "\n");
+    System.out.println("\nShould print \"hi\": \n" + list.set(10, "bye")); //should return "hi"
     System.out.println(list);
 
 
     System.out.println("---------------------- \n");
     System.out.println("TESTS FOR PHASE 3");
-    
+
+    System.out.println("Testing contains():");
+    System.out.println("\nShould return true:");
+    System.out.println(list.contains("hi"));
+    System.out.println(list.contains("bye"));
+    System.out.println("\n Should return false:");
+    System.out.println(list.contains("hello"));
+    System.out.println(list.contains(null));
 
 /*    for (int x = 0; x < 100; x++) {
       list.add("hi");
